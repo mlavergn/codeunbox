b3a42d4fb258e6c6136bfccee60081b864b91a3b
Marc Lavergne <lavergne@apple.com>
2013-10-13 00:55:22 -0700

    Fix method names / start Dash export

--- codeunbox.py
+++ codeunbox.py
@@ -29,7 +29,48 @@ class Base:
 
   #-----------------------------------------------------------------------------
 
-  def import(self, file):
+  def doDashExport(self):
+    data = {}
+    for key in self.snippets:
+      snippet = self.snippets[key]
+      data[key] = {}
+      data[key]['title'] = snippet['title']
+      for assetkey in snippet['rel_a']:
+        asset = self.assets[assetkey]
+        data[key]['data'] = "Note: %s\nPath: %s\n--\n\n%s" % (asset['note'].decode("utf-8", "replace"), asset['path'].decode("utf-8", "replace"), asset['content'].decode("utf-8", "replace"))
+        if not data[key].has_key('syntax') and asset['syntax'] is not None:
+          data[key]['syntax'] = snippet['syntax']
+      for tagkey in snippet['rel_t']:
+        tag = self.tags[tagkey]
+        data[key]['tags'] = tag['title']
+
+      if not data[key].has_key('syntax'):
+        data[key]['syntax'] = 'Standard'
+
+  #-----------------------------------------------------------------------------
+
+  def extToSyntax(self, ext):
+    syntax = None
+    if ext == 'c':
+      syntax = 'C'
+    elif ext == 'cc':
+      syntax = 'C++'
+    elif ext == 'm':
+      syntax = 'Objective-C'
+    elif ext == 'py':
+      syntax = 'Python'
+    elif ext == 'txt':
+      syntax = 'Standard'
+    elif ext == 'sh':
+      syntax = 'Shell'
+    elif ext == 'scpt':
+      syntax = 'Applescript'
+
+    return syntax
+
+  #-----------------------------------------------------------------------------
+
+  def doImport(self, file):
     fd = open(os.path.expanduser(file))
     data = fd.read()
     fd.close()
@@ -42,9 +83,11 @@ class Base:
       xid = xobject.getAttribute('id')
       xtype = xobject.getAttribute('type')
 
-      xname = ""
-      xpath = ""
       xdata = ""
+      xpath = ""
+      xsyntax = None
+      xname = ""
+      xnote = ""
 
       # parse the object attribute children
       attribs = xobject.getElementsByTagName('attribute')
@@ -55,8 +98,12 @@ class Base:
           xdata = self.decode(bdata)
         if bname == 'path':
           xpath = attrib.childNodes[0].nodeValue
+          ext = os.path.splitext(xpath)
+          xsyntax = self.extToSyntax(ext)
         if bname == 'name':
           xname = attrib.childNodes[0].nodeValue
+        if bname == 'notes':
+          xnote = attrib.childNodes[0].nodeValue
 
       # parse the object relations children
       xrel_s = ""
@@ -84,6 +131,8 @@ class Base:
           self.assets[xid] = {}
         self.assets[xid]['content'] = xdata
         self.assets[xid]['path'] = xpath
+        self.assets[xid]['note'] = xnote
+        self.assets[xid]['syntax'] = xsyntax
         self.assets[xid]['rel_s'] = xrel_s.split()
         self.assets[xid]['rel_t'] = xrel_t.split()
       elif xtype == 'TAG':
@@ -114,6 +163,7 @@ class Base:
 
 if __name__ == "__main__":
   base = Base(0)
-  base.import("~/Dropbox/CodeBox.cbxml")
+  base.doImport("~/Dropbox/CodeBox.cbxml")
+  base.doDashExport()
 
 #===============================================================================
